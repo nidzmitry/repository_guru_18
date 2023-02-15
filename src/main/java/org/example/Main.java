73f8d56dfe54646e0ad2be51555a6c6fa23d8966
Dzmitry <dmitry@MacBook-Pro-Dzmitry.local>
2023-02-16 00:08:21 +0300

    add SOUT

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello all!");
+        System.out.println("Hello!");
+        System.out.println("How are u?");
+
     }
 }
